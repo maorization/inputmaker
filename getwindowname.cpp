24053b8eb5ce810d5a3e0e882b73cdaaa6c03219
Maor Nizer <maorization@gmail.com>
2021-08-24 23:27:38 -0700

    a

---
+++ getwindowname.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+using std::string;
+
+string GetActiveWindowTitle()
+{
+ char wnd_title[256];
+ HWND hwnd=GetForegroundWindow(); // get handle of currently active window
+ GetWindowText(hwnd,wnd_title,sizeof(wnd_title));
+    std::cout << wnd_title << std::endl;
+ return wnd_title;
+}
+
+int main()
+{
+     
+    GetActiveWindowTitle();
+    
+}
