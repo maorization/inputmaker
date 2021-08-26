53612504fe2f250800fb5cccd0f6caf50b13898c
Maor Nizer <maorization@gmail.com>
2021-08-26 11:03:38 -0700

    a

--- functionbot.cpp
+++ functionbot.cpp
@@ -4,28 +4,61 @@
 
 DWORD keybindA = 0x41;
 DWORD keybindC = 0x43;
-DWORD keybindD = 0x44;
+DWORD d = 0x44;
 DWORD keybindV = 0x56;
 DWORD keybindCTRL = 0x11;
-DWORD keybindALT = 0x12;
+DWORD ALT = 0x12;
 DWORD keybindTAB = 0x09;
 DWORD keybindWIN = 0x5B;
-DWORD x = 0x41;
+DWORD x = 0x58;
 
 void keyStroke(DWORD x) {
     INPUT ip = {};
 
-    // press D
+    // press X
     ip.type = INPUT_KEYBOARD;
 	ip.ki.wVk = x; // virtual-key code for the key
 	SendInput(1, &ip, sizeof(INPUT));
 
-    // release D
+    // release X
 	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
 	SendInput(1, &ip, sizeof(INPUT));
 
 };
 
+void keyStroke(DWORD x) {
+    INPUT ip = {};
+
+    // press X
+    ip.type = INPUT_KEYBOARD;
+	ip.ki.wVk = x; // virtual-key code for the key
+	SendInput(1, &ip, sizeof(INPUT));
+
+    // release X
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
+
+};
+
+void holdALT() {
+    INPUT ip = {};
+
+    // press D
+    ip.type = INPUT_KEYBOARD;
+	ip.ki.wVk = ALT; // virtual-key code for the key
+	SendInput(1, &ip, sizeof(INPUT));
+
+
+};
+
+void releaseALT() {
+    // release ALT
+    ip.ki.wVk = ALT;
+    ip.ki.dwFlags = KEYEVENTF_KEYUP;
+    // 
+    SendInput(1, &ip, sizeof(INPUT));
+
+}
 
 int main() {
 
