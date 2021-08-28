10e6a1df0c1c421217a6aca9eb83688cd24f4396
Maor Nizer <maorization@gmail.com>
2021-08-27 21:22:41 -0700

    a

--- funcnew.cpp
+++ funcnew.cpp
@@ -1,19 +1,33 @@
+#define UNICODE
 #include <iostream>
+#include <windows.h>
 #include <string>
-
 using namespace std;
 
-string XXX = "test";
+DWORD D = 0x44;
+DWORD A = 0x41;
+DWORD C = 0x43;
+DWORD V = 0x56;
 
-void setString(string y) {
-    string text ;
-    text = "hello";
-    cout << text << ' ';
+void pressButton(const DWORD &i) {
+    cout << i << endl;
+    INPUT ip = {};
+
+    // press D
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wVk = i;
+    SendInput(1, &ip, sizeof(INPUT));
+
+	ip.ki.dwFlags = KEYEVENTF_KEYUP; // KEYEVENTF_KEYUP for key release
+	SendInput(1, &ip, sizeof(INPUT));
 }
-    
 
 
 
-int main(){
-    setString();
+int main()
+{
+
+Sleep(2000);
+pressButton(D);
+
 }
